dce6cc619e92a8eba47c309a05099fce089d875c
Jatin Sharma <112959846+JatSh1804@users.noreply.github.com>
2024-11-10 13:34:43 +0000

    Asset Route Update

--- apps/nextjs-example/next.config.mjs
+++ apps/nextjs-example/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   transpilePackages: ["wallet-adapter-react", "wallet-adapter-plugin"],
-  assetPrefix: isProd ? "/aptos-wallet-adapter" : "",
+  // assetPrefix: isProd ? "/aptos-wallet-adapter" : "",
   basePath: isProd ? "/aptos-wallet-adapter" : "",
   webpack: (config) => {
     config.resolve.fallback = { "@solana/web3.js": false };
